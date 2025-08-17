99b4abcbc84deaf504ae01648103df85d73d69ff
Vadym Kryvenko <vadymkryvenko@gmail.com>
2025-08-17 15:41:42 +0200

    add second task

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,8 @@
-
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
+    return message;
+}
 
 
 
